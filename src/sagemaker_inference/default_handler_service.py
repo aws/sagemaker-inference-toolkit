8efb1672798d747cd623e5dd2eb7919af87a1b80
akulk314 <946235+akulk314@users.noreply.github.com>
2020-10-13 15:25:06 -0400

    change: upgrade MMS version and update command (#71)
    Co-authored-by: akulk314 <akulk314@users.noreply.github.com>
    Co-authored-by: Ajay Karpur <akarpur@amazon.com>

--- src/sagemaker_inference/default_handler_service.py
+++ src/sagemaker_inference/default_handler_service.py
@@ -27,7 +27,7 @@ class DefaultHandlerService(object):
         - The ``handle`` method is invoked for all incoming inference requests to the model server.
         - The ``initialize`` method is invoked at model server start up.
 
-    Implementation of: https://github.com/awslabs/mxnet-model-server/blob/master/docs/custom_service.md
+    Implementation of: https://github.com/awslabs/multi-model-server/blob/master/docs/custom_service.md
     """
 
     def __init__(self, transformer=None):
