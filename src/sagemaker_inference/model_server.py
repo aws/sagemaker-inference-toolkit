08a1fc1f8ab15122bf295ebbf0604e72c3ea10ab
Hakan Baba <ahakanbaba@gmail.com>
2020-10-29 20:45:02 -0700

    fix: Add a noop change
    This is to see how the default passing integration tests behave .

--- src/sagemaker_inference/model_server.py
+++ src/sagemaker_inference/model_server.py
@@ -10,7 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-"""This module contains functionality to configure and start the
+"""This module  contains functionality to configure and start the
 multi-model server."""
 from __future__ import absolute_import
 
