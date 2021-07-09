7fcb80561c0d87cf035e194a86f9fa5c6c9d9b33
Abdelrahman Maharek <10356905+amaharek@users.noreply.github.com>
2021-07-09 19:45:31 +0100

    fix: Fixing issue #82 (#83)

--- src/sagemaker_inference/model_server.py
+++ src/sagemaker_inference/model_server.py
@@ -150,6 +150,7 @@ def _generate_mms_config_properties():
         "default_workers_per_model": env.model_server_workers,
         "inference_address": "http://0.0.0.0:{}".format(env.inference_http_port),
         "management_address": "http://0.0.0.0:{}".format(env.management_http_port),
+        "vmargs": "-XX:-UseContainerSupport",
     }
 
     custom_configuration = str()
